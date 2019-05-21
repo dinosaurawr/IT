c90282886f5615bcc5243a15b60b1ae260d4e29d
dinosaurawr <dinosaurawr.1@gmail.com>
2019-05-21 17:46:18 +0300

    task1 updated

--- Lab5/task1.c
+++ Lab5/task1.c
@@ -16,12 +16,31 @@ char* find(const char* str, const char* substr) {
 	for (int i = 0; i <= lenStr - lenSub; ++i) {
 		int j;
 		for (j = 0; str[i + j] == substr[j]; ++j);
-		if (j - lenSub == 1 && i == lenStr - lenSub) return i;
+		if (j - lenSub == 1 && i == lenStr - lenSub) return &str[i];
 
 	}
 	return 0;
 }
 
 void delete(char* str, char* substr) {
+	char* firstSymb = find(str, substr);
+	char* lastSymb = find(str, substr) + strlen(substr);
+	for (char* curr = find(str, substr);  curr!=lastSymb;++curr) {
+		*curr = "0";
+	}
+}
 
+void strcpy(char* dest, int len, const char* src) {
+
+	if (strlen(src) > len) {
+		int i;
+		for (i = 0; i < len - 1; ++i) {
+			dest[i] = src[i];
+		}
+		dest[i + 1] = "\0";
+	}
+	else
+	{
+		while ((*dest++ = *src++) != '\0');
+	}
 }
